3290febe3a344e212d50114207f15c16a6bcf9c3
TakingTheL <yliu6@stuy.edu>
2018-03-25 21:46:14 -0400

    progress?

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-//GDP, surplus/deficit, 
-var budget1999 = [11.9, -36.1]
-var budget1940 = [1.21, 157.0]
+//GDP, total revenues, total spending, surplus/deficit 
+//in billions of USD
+var budget1999 = [11900, 2290, 2130, -36.1]
+var budget2016 = [16500, 2990, 3540, -552 ]
+var data = budget2016;
+
+var toggleButton = document.getElementById("toggleButton"); //switch between 1999 and 2016
+var toggle = 2016;
+var toggleFunc = function(){
+    if (toggle == 2016){
+	toggle = 1999;
+	data = budget1999;
+	toggleButton.innerHTML = "See 2016";
+    } else {
+	toggle = 2016;
+	data = budget2016;
+	toggleButton.innerHTML = "See 1999";
+    }
+    drawChart();
+}
+
+toggleButton.addEventListener( "click", toggleFunc);
+
+var drawChart = function(d){
+    d3.select("chart")
+	.selectAll("div")
+	.data(data)
+	.enter().append("div")
+	.style("width", function(d) {return d *10 + "px";})
+	.text(function(d){return d;});
+}
