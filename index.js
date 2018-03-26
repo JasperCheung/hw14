4a19cfdcf9999355c57854ba04a998f47e5f54f4
jaspercheung <jcheung6@stuy.edu>
2018-03-25 22:34:20 -0400

    fixed

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var budget2016 = [16500, 2990, 3540, -552 ]
 var data = budget2016;
 
 var toggleButton = document.getElementById("toggleButton"); //switch between 1999 and 2016
+console.log(toggleButton);
 var toggle = 2016;
 var toggleFunc = function(){
     if (toggle == 2016){
@@ -15,17 +16,30 @@ var toggleFunc = function(){
 	toggle = 2016;
 	data = budget2016;
 	toggleButton.innerHTML = "See 1999";
-    }
+    }	
+    clearChart();
     drawChart();
 }
 
 toggleButton.addEventListener( "click", toggleFunc);
 
 var drawChart = function(d){
-    d3.select("chart")
+    d3.select(".chart")
 	.selectAll("div")
 	.data(data)
 	.enter().append("div")
-	.style("width", function(d) {return d *10 + "px";})
+	.transition().duration(1000)
+	.style("width", function(d) {
+	    if(d < 0){
+		return d * -1 + "px";
+	    }
+	    return d *.1 + "px";})
 	.text(function(d){return d;});
+}
+
+
+var clearChart = function(d){
+    d3.select(".chart")
+	.selectAll("div")
+	.remove()
 }
